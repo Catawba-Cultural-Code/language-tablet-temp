deae7e9601e30014cff41dcc3d22c614f64d0e8f
delesslin <delesslingw@gmail.com>
2023-08-07 10:38:26 -0400

    :rocket: adding timed reload to homescreen

--- App.js
+++ App.js
@@ -1,14 +1,52 @@
+const HOME_URL = 'https://catawbalanguage.org/'
+const DELAY = 2 * 60 * 1000
 import { WebView } from 'react-native-webview'
 import { StatusBar } from 'expo-status-bar'
-import { View } from 'react-native'
+import { Text, TouchableOpacity } from 'react-native'
+import useTimeout from './useTimeout'
+import { useState } from 'react'
 export default function App() {
+  const [key, setKey] = useState(0)
+  const [example, setExample] = useState('no timer')
+  const [currentURL, setCurrentURL] = useState(HOME_URL)
+  const [restart, clear, isActive] = useTimeout(
+    () => {
+      setExample('Timed out!')
+      if (currentURL !== HOME_URL) {
+        setKey((i) => i + 1)
+      }
+    },
+    DELAY,
+    false
+  )
+  const handleNavigation = ({ url }) => {
+    // console.log(url)
+    if (url !== HOME_URL) {
+      setCurrentURL(url)
+      restart()
+      setExample('setting timer')
+    }
+  }
+  const handlePress = () => {
+    if (currentURL !== HOME_URL) {
+      setExample('resetting timer')
+      restart()
+    }
+  }
   return (
-    <View style={{ flex: 1 }}>
+    <TouchableOpacity
+      activeOpacity={1}
+      style={{ flex: 1 }}
+      onPress={handlePress}
+    >
+      {/* <Text>{example}</Text> */}
       <WebView
+        key={key}
         style={{ flex: 1 }}
-        source={{ uri: 'https://catawbalanguage.org' }}
+        source={{ uri: HOME_URL }}
+        onNavigationStateChange={handleNavigation}
       />
       <StatusBar hidden={true} />
-    </View>
+    </TouchableOpacity>
   )
 }
